a083d808602b18d49f9f93c437e47e87e818768a
hemanthkammara <hemanthcr217@gmail.com>
2023-11-02 02:40:07 +0530

    protfolio

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,17 +2,18 @@ import React from "react";
 
 
 import myimg1 from "../assests/my img1.png";
+import resume from "../assests/certificate.pdf";
 import { Link } from "react-router-dom";
 
 function Home() {
 
-  // const resumedown2 = () => {
-  //   window.open(
-  //     "https://drive.google.com/file/d/1UBgt4cfEWrtO6DkeT8boJlNJqLDL3kG/view?usp=sharing",
-  //     "_blank"
+  const resumedown2 = () => {
+    window.open(
+      "https://drive.google.com/file/d/1wknVsNofpK-WkVndbvECcUQIFlQczKep/view?usp=sharing",
+      "_blank"
       
-  //   );
-  // };
+    );
+  };
 
 
 
@@ -25,18 +26,38 @@ function Home() {
         <h2>Hey 👋 there! </h2>
         <h1 id="user-detail-name">I am Hemanth</h1>
         <h2>Full Stack Web Developer.</h2>
-        <Link
+        {/* <Link
             className="nav-link resume"
-            // to="https://drive.google.com/uc?export=download&id=1UBgt4cfEWrtO6DkeT8boJlXNJqLDL3kG"
+            //to=""
           >
             <button
               id="resume-button-2"
               
-              // onClick={resumedown2}
+              onClick={resumedown2}
             >
               resume
             </button>
-          </Link>
+          </Link> */}
+
+<Link
+         
+          className="nav-link resume"
+          to="https://drive.google.com/file/d/1wknVsNofpK-WkVndbvECcUQIFlQczKep/view?usp=sharing"
+          target="_blank"
+        >
+          <button
+            id="resume-button-2"
+            onClick={() => {
+              const link = document.createElement("a");
+              link.href = resume;
+              link.setAttribute("download", "Hemanth-Kumar-Resume.pdf");
+              link.click();
+            }}
+          >
+            <i style={{ marginRight: "5px" }} className="far fa-file-alt"></i>
+            Resume
+          </button>
+        </Link>
       </div>
       <div >
         <img className="home-img" src={myimg1} alt="" />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,23 +3,24 @@ import AllRoutes from "./AllRoutes";
 import { Link } from "react-router-dom";
 import { MdMenu } from "react-icons/md";
 import { FaXmark } from "react-icons/fa6";
+import resume from "../assests/certificate.pdf";
 
 function Navbar() {
   const [show, setshow] = useState(true);
-  const [display, sedisplay] = useState(true)
-  const [currentPath, setCurrentPath] = useState(window.location.hash)
+  const [display, sedisplay] = useState(true);
+  const [currentPath, setCurrentPath] = useState(window.location.hash);
 
   const toggle = () => {
     setshow(!show);
-    sedisplay(!display)
+    sedisplay(!display);
   };
 
   const goTo = (activeurl) => {
     if (activeurl) {
-      setCurrentPath(activeurl)
+      setCurrentPath(activeurl);
     }
     setshow(true);
-    sedisplay(true)
+    sedisplay(true);
   };
 
   // const resumedown = () => {
@@ -32,30 +33,60 @@ function Navbar() {
   return (
     <div className="navbar" id="nav-menu">
       <div>
-        <h1>hemanth</h1>
+        <h1>Hemanth</h1>
       </div>
       <button className="menu-btn" onClick={toggle}>
-        {show ? <MdMenu/> : <FaXmark/>}
+        {show ? <MdMenu /> : <FaXmark />}
       </button>
 
       <div className="links" id={display ? "close" : "open"}>
-        <a onClick={()=>goTo('#home')} className={`nav-link home ${currentPath == "#home" ? "link-active" : ""}`} href="#home">
+        <a
+          onClick={() => goTo("#home")}
+          className={`nav-link home ${
+            currentPath == "#home" ? "link-active" : ""
+          }`}
+          href="#home"
+        >
           Home
         </a>
-        <a onClick={()=>goTo('#about')} className={`nav-link about ${currentPath == "#about" ? "link-active" : ""}`} href="#about">
+        <a
+          onClick={() => goTo("#about")}
+          className={`nav-link about ${
+            currentPath == "#about" ? "link-active" : ""
+          }`}
+          href="#about"
+        >
           About
         </a>
-        <a onClick={()=>goTo('#skills')} className={`nav-link skills ${currentPath == "#skills" ? "link-active" : ""}`} href="#skills">
+        <a
+          onClick={() => goTo("#skills")}
+          className={`nav-link skills ${
+            currentPath == "#skills" ? "link-active" : ""
+          }`}
+          href="#skills"
+        >
           Skills
         </a>
-        <a onClick={()=>goTo('#projects')} className={`nav-link projects ${currentPath == "#projects" ? "link-active" : ""}`} href="#projects">
+        <a
+          onClick={() => goTo("#projects")}
+          className={`nav-link projects ${
+            currentPath == "#projects" ? "link-active" : ""
+          }`}
+          href="#projects"
+        >
           Projects
         </a>
-        <a onClick={()=>goTo('#contact')} className={`nav-link contact ${currentPath == "#contact" ? "link-active" : ""}`} href="#contact">
+        <a
+          onClick={() => goTo("#contact")}
+          className={`nav-link contact ${
+            currentPath == "#contact" ? "link-active" : ""
+          }`}
+          href="#contact"
+        >
           Contact
         </a>
         {/* <a  className='nav-link resume' id='resume-button-1' href="#resume">Resume</a> */}
-        <Link onClick={()=>goTo(false)} 
+        {/* <Link onClick={()=>goTo(false)} 
           className="nav-link resume"
           // to="https://drive.google.com/uc?export=download&id=1UBgt4cfEWrtO6DkeT8boJlXNJqLDL3kG"
         >
@@ -72,7 +103,41 @@ function Navbar() {
             <i style={{ marginRight: "5px" }} className="far fa-file-alt"></i>
             resume
           </button>
+        </Link> */}
+
+        <Link
+          onClick={() => goTo(false)}
+          className="nav-link resume"
+          to="https://drive.google.com/file/d/1wknVsNofpK-WkVndbvECcUQIFlQczKep/view?usp=sharing"
+          target="_blank"
+        >
+          <button
+            id="resume-button-1"
+            onClick={() => {
+              const link = document.createElement("a");
+              link.href = resume;
+              link.setAttribute("download", "Hemanth-Kumar-Resume.pdf");
+              link.click();
+            }}
+          >
+            <i style={{ marginRight: "5px" }} className="far fa-file-alt"></i>
+            Resume
+          </button>
         </Link>
+
+        {/* <a className="nav-link resume"
+         href=""
+          
+          target="_blank"
+          onClick={() => {
+            const link = document.createElement("a");
+            link.href = resume;
+            link.setAttribute("download", "hemanth_kumar_resume.pdf");
+            link.click();
+          }}
+          >
+         resume
+          </a> */}
       </div>
     </div>
   );
